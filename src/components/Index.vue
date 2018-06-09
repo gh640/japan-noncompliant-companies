d3711d8d9c0c3ed4d7107f7da20f507729e381bc
Goto Hayato <habita.gh@gmail.com>
2018-06-09 12:39:44 +0900

    Issue #2: 存在する tsv の選択肢をすべて追加。

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <select class="tsv-selector" v-model="tsv">
-      <option disabled value="">- 選択してください -</option>
+      <option disabled value="">- 期間を選択してください -</option>
       <option
         v-for="(tsvValue, tsvLabel) in tsvOptions"
         :key="tsvValue"
@@ -59,7 +59,14 @@ export default {
     return {
       msg: '',
       tsvOptions: {
+        '2016/10 - 2017/09': 'company_list_20161001_to_20170930.tsv',
+        '2016/11 - 2017/10': 'company_list_20161101_to_20171031.tsv',
+        '2016/12 - 2017/11': 'company_list_20161201_to_20171130.tsv',
+        '2017/01 - 2017/12': 'company_list_20170101_to_20171231.tsv',
         '2017/02 - 2018/01': 'company_list_20170201_to_20180131.tsv',
+        '2017/03 - 2018/02': 'company_list_20170301_to_20180228.tsv',
+        '2017/04 - 2018/03': 'company_list_20170401_to_20180331.tsv',
+        '2017/05 - 2018/04': 'company_list_20170501_to_20180430.tsv',
       },
     };
   },
