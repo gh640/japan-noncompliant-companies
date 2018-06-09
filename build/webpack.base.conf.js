62f51c7f9738e72a0ddf6c93da578165c6b35a1b
Goto Hayato <habita.gh@gmail.com>
2018-06-09 14:22:28 +0900

    Issue #10: 不要な `csv-loader` の設定を `webpack.base.conf.js` から削除。

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,15 +74,6 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      },
-       {
-        test: /\.tsv$/,
-        loader: 'csv-loader',
-        options: {
-          dynamicTyping: true,
-          header: true,
-          skipEmptyLines: true
-        }
       }
     ]
   },
